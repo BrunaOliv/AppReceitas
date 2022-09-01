66d208b36689446fee54f141d39e574dc3b75f2d
Bruna Oliveira <bruna.oliveira@programmers.com.br>
2022-09-01 20:56:59 -0300

    criacao class categoria

---
+++ AppReceitasDomain/Entities/Receitas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReceitas.Domain.Entities
+{
+    public class Receitas
+    {
+        public string Name { get; set; }
+        public string Ingredientes { get; set; }
+        public string ModoDePreparo { get; set; }
+        public string  Imagem { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
