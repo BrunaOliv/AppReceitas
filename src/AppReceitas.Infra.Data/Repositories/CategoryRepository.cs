ae74da11c6af8778fd388463a1ae8ecabee0f607
Bruna Oliveira <bruna.oliveira@programmers.com.br>
2022-09-04 09:45:14 -0300

    add CategoryRepository

---
+++ src/AppReceitas.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using AppReceitas.Domain.Entities;
+using AppReceitas.Domain.Interfaces;
+using AppReceitas.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReceitas.Infra.Data.Repositories
+{
+    public class CategoryRepository: ICategoryRepository 
+    {
+        ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
