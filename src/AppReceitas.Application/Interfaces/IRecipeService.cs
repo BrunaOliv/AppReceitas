306a8f05ad750c4ae221e4fa23ce631ff7418bd7
Bruna Oliveira <bruna.oliveira@programmers.com.br>
2022-09-05 15:02:06 -0300

    add service recipes

---
+++ src/AppReceitas.Application/Interfaces/IRecipeService.cs
@@ -0,0 +1,19 @@
+﻿using AppReceitas.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReceitas.Application.Interfaces
+{
+    public interface IRecipeService
+    {
+        Task<IEnumerable<RecipeDTO>> GetRecipes();
+        Task<RecipeDTO> GetById(int? id);
+        Task<RecipeDTO> GetRecipeCategory(int? id);
+        Task Add(RecipeDTO recipeDTO);
+        Task Update(RecipeDTO recipeDTO);
+        Task Remove(int? id);
+    }
+}
---
+++ src/AppReceitas.Application/Services/RecipeService.cs
@@ -0,0 +1,60 @@
+﻿using AppReceitas.Application.DTOs;
+using AppReceitas.Application.Interfaces;
+using AppReceitas.Domain.Entities;
+using AppReceitas.Domain.Interfaces;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReceitas.Application.Services
+{
+    public class RecipeService: IRecipeService
+    {
+        private readonly IRecipesRepository _recipesRepository;
+        private readonly IMapper _mapper;
+        public RecipeService(IRecipesRepository recipesRepository, IMapper mapper)
+        {
+            _recipesRepository = recipesRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(RecipeDTO recipeDTO)
+        {
+            var recipeEntity = _mapper.Map<Recipes>(recipeDTO);
+            await _recipesRepository.CreateAsync(recipeEntity);
+        }
+
+        public async Task<RecipeDTO> GetById(int? id)
+        {
+            var recipeEntity = await _recipesRepository.GetByIdAsync(id);
+            return _mapper.Map<RecipeDTO>(recipeEntity);
+        }
+
+        public async Task<RecipeDTO> GetRecipeCategory(int? id)
+        {
+            var recipeEntity = await _recipesRepository.GetRecipesCategoryAsync(id);
+            return _mapper.Map<RecipeDTO>(recipeEntity);
+        }
+
+        public async Task<IEnumerable<RecipeDTO>> GetRecipes()
+        {
+            var recipeEntity = await _recipesRepository.GetRecipesAsync();
+            return _mapper.Map<RecipeDTO[]>(recipeEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var recipeEntity = _recipesRepository.GetByIdAsync(id).Result;
+            await _recipesRepository.RemoveAsync(recipeEntity);
+        }
+
+        public async Task Update(RecipeDTO recipeDTO)
+        {
+            var recipeEntity = _mapper.Map<Recipes>(recipeDTO);
+            await _recipesRepository.UpdateAsync(recipeEntity);
+        }
+    }
+}
