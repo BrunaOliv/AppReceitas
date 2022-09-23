390a5cd3ebaa04e6e67d7f8a89d3d9361a12a0d4
Bruna Oliveira <bruna.oliveira@programmers.com.br>
2022-09-23 15:20:32 -0300

    add rotas

--- src/app/appReceitas/src/app/app-routing.module.ts
+++ src/app/appReceitas/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroReceitaComponent } from './modules/cadastro-receita/cadastro-receita.component';
+import { ReceitasComponent } from './modules/receitas/receitas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ReceitasComponent },
+    { path: 'cadastro', component: CadastroReceitaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
